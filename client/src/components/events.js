a8ef0c3ddab1fb17ce7edfe07a63f339a74a3019
Dana <dabinkim807@gmail.com>
2023-03-16 18:34:24 -0400

    add routes in events.js

--- client/src/components/events.js
+++ client/src/components/events.js
@@ -17,10 +17,65 @@ function Events() {
 	useEffect(() => {getRequest()}, []);
 
 	// post request
+	const postRequest = (input) => {
+		fetch(`http://localhost:8080/api/events`, {
+			method: "POST",
+			headers: {
+				"Content-type": "application/JSON"
+			},
+			// body: JSON.stringify({ question: totalQuestions[currentQAndA].question, answer: selectedAnswer })
+		})
+			.then((response) => response.json())
+			.then((result) => {
+				console.log(result);
+				// setValidated(result);
+
+				// if (result.isCorrect) {
+				// 	setScore(score + 1);
+				// }
+
+			});
+	}
 
 	// put request
+	const putRequest = (input) => {
+		fetch(`http://localhost:8080/api/events/:eventID`, {
+			method: "PUT",
+			headers: {
+				"Content-type": "application/JSON"
+			},
+			// body: JSON.stringify({ question: totalQuestions[currentQAndA].question, answer: selectedAnswer })
+		})
+			.then((response) => response.json())
+			.then((result) => {
+				console.log(result);
+				// setValidated(result);
 
+				// if (result.isCorrect) {
+				// 	setScore(score + 1);
+				// }
+
+			});
+	}
 	// delete request
+	const deleteRequest = (input) => {
+		fetch(`http://localhost:8080/api/events/:eventID`, {
+			method: "DELETE",
+			headers: {
+				"Content-type": "application/JSON"
+			},
+		})
+			.then((response) => response.json())
+			.then((result) => {
+				console.log(result);
+				// setValidated(result);
+
+				// if (result.isCorrect) {
+				// 	setScore(score + 1);
+				// }
+
+			});
+	}
 
 	const toggleCards = () => {
 		if (events.length === 0) {
