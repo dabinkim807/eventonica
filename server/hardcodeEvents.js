0ae563fa70c75393c734121a089ba6b8e7d85ac2
Dana <dabinkim807@gmail.com>
2023-03-16 15:06:14 -0400

    import hardcoded data to reuse in server.js

---
+++ server/hardcodeEvents.js
@@ -0,0 +1,50 @@
+const hardcodeEvents = [
+  {
+    id: 1,
+    name: "Women in Tech Techtonica Panel",
+    date: "10/10/23",
+    description: "Overland Park Convention Center",
+    category: "Personal",
+    favorite: true
+  },
+  {
+    id: 2,
+    name: "Japanese Cultural Education",
+    date: "10/10/23",
+    description: "Seattle Convention Center",
+    category: "Work",
+    favorite: false
+  },
+  {
+    id: 3,
+    name: "Haven 90's Party Night Club",
+    date: "10/10/23",
+    description: "Hilton Hotel Kansas City",
+    category: "Personal",
+    favorite: false
+  },
+  {
+    id: 4,
+    name: "Comedy Night at the Station",
+    date: "10/10/23",
+    description: "SF Hilton Hotel",
+    category: "Personal",
+    favorite: false
+  },
+  { id: 5, 
+    name: "A Decadent Arts Experience", 
+    date: "10/10/23",
+    description: "West Ridge Mall",
+    category: "Personal",
+    favorite: true
+  },
+  { id: 6, 
+    name: "Techtonica Classroom Course", 
+    date: "10/10/23",
+    description: "Techtonica HQ",
+    category: "Work",
+    favorite: false
+  }
+]
+
+module.exports = hardcodeEvents;
